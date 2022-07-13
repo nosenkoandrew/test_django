4e38613db73fe1957b8951eba55555d57be4846b
Andrew Nosenko <andrewnoa20@gmail.com>
2022-07-13 18:49:51 +0300

    test commit with import error 4

--- myapp/manage.py
+++ myapp/manage.py
@@ -7,7 +7,7 @@ import django  # noqa # pylint: disable=unused-import # fmt: off # isort: skip
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,5 +19,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
